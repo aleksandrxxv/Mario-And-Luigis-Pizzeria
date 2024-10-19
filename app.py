1a7c599b2e5f7fc59bffa3172551717cd6d83681
Adrian <adrian.i.aleksandrov@gmail.com>
2024-10-19 12:06:27 +0200

    Added basic logic to the menu page

--- app.py
+++ app.py
@@ -1,7 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+import json
 
+db = SQLAlchemy()
 app = Flask(__name__)
 app.secret_key = "Secret key"
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres.dpznsagmfkgblxagagdm:YR*QgPD9n.k#u9x@aws-0-eu-north-1.pooler.supabase.com:6543/postgres'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
+class MenuItem(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    PizzaName = db.Column(db.String(100))
+    PizzaPrice = db.Column(db.Float)
+    PizzaToppings = db.Column(db.Text)
+    PizzaImage = db.Column(db.Text)
+
+    def get_toppings_list(self):
+        print(self.PizzaToppings)
+        return json.loads(self.PizzaToppings) if self.PizzaToppings else []
+
+
+with app.app_context():
+    db.create_all()
 
 
 @app.route('/')
@@ -10,4 +33,5 @@ def index():
 
 @app.route('/menu')
 def menu():
-    return render_template('menu.html')
+    all_items = MenuItem.query.all()
+    return render_template('menu.html', items = all_items)
